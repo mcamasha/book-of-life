fc9d59c74bddcfd115c802878963bad1c7fa99d7
Сафарова Мария Алексеевна <Safarova.M.A@sberbank.ru>
2020-07-17 11:34:47 +0300

    Updates

---
+++ precache-manifest.ca13315fb5634b892511b5bfd7895eac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c81b19de3c6d6f14753242b323eca24",
+    "url": "/book-of-life/index.html"
+  },
+  {
+    "revision": "e19d19c5e76caf46d2ca",
+    "url": "/book-of-life/static/css/main.491bee12.chunk.css"
+  },
+  {
+    "revision": "b613e49d98662ec6fe95",
+    "url": "/book-of-life/static/js/2.8edbc5fb.chunk.js"
+  },
+  {
+    "revision": "e19d19c5e76caf46d2ca",
+    "url": "/book-of-life/static/js/main.ca84bcf2.chunk.js"
+  },
+  {
+    "revision": "d039ce0ccb0c7e6d556a",
+    "url": "/book-of-life/static/js/runtime-main.ce8fea5e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/book-of-life/static/media/logo.25bf045c.svg"
+  }
+]);
